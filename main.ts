da07f16c8933264da252e100069e588b9cb8a5cd
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-06-19 10:13:08 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,64 @@
+let player2 = sprites.create(img`
+. . 5 5 . .
+. 5 5 5 5 .
+. 5 5 5 5 . 
+. . 5 5 . . 
+`, SpriteKind.Player)
+
+let nemeny = sprites.create(img`
+    . 2 2 2 .
+    2 2 2 2 2
+    2 2 2 2 2
+    . 2 2 2 .
+`, SpriteKind.Enemy)
+
+controller.moveSprite(player2, 69, 69)
+
+forever(function() {
+    let gazornogaz = randint(1, 69)
+    nemeny.follow(player2, gazornogaz)
+    console.log(gazornogaz)
+    pause(1000)
+})
+
+nemeny.setPosition(130, 0)
+scene.cameraFollowSprite(player2)
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(player2, nemeny) {
+    game.gameOver(false)
+})
+
+tiles.setCurrentTilemap(tilemap`level1`)
+tiles.setCurrentTilemap(tilemap`level4`)
+tiles.setCurrentTilemap(tilemap`leveltest`)
+
+let coins = 0
+let coin: Sprite = null
+
+namespace SpriteKind {
+    export const Coin = SpriteKind.create()
+}
+    
+function spawnCoin() {
+    let coin = sprites.create(img`
+            . . 4 4 . .
+            . 4 f f 4 .
+            4 4 f 4 4 4
+            . 4 f f 4 .
+            . . 4 4 . .
+    `, SpriteKind.Coin)
+    tiles.placeOnRandomTile(coin, sprites.castle.tileGrass2)
+}
+
+for (let i = 0; i < 5; i++) {
+    spawnCoin()
+}
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function(player2, coin){
+    coin.destroy()
+    coins +=1
+    if (coins >= 10){
+        game.over(true)
+    }
+    spawnCoin()
+})
