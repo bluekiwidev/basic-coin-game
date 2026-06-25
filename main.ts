b57b346a46f091c0f95f3e7f593f6116438d306e
bluekiwidev <111331826+bluekiwidev@users.noreply.github.com>
2026-06-26 10:19:00 +1200

    fixed n broke

--- main.ts
+++ main.ts
@@ -1,58 +1,93 @@
-namespace SpriteKind {
-    export const Coin = SpriteKind.create()
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (player2, nemeny) {
-    game.gameOver(false)
-})
-function spawnCoin () {
-coin2 = sprites.create(img`
-        . . 4 4 . . 
-        . 4 f f 4 . 
-        4 4 f 4 4 4 
-        . 4 f f 4 . 
-        . . 4 4 . . 
-        `, SpriteKind.Coin)
-    tiles.placeOnRandomTile(coin2, sprites.castle.tileGrass2)
-}
-function rotateLevel () {
-    tiles.setCurrentTilemap(tilemap(randint(1, 5)))
-}
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function (player2, coin) {
-    coin.destroy()
-    coins += 1
-})
-let gazornogaz = 0
-let coins = 0
-let coin2: Sprite = null
 let player2 = sprites.create(img`
-    . . 5 5 . . 
-    . 5 5 5 5 . 
-    . 5 5 5 5 . 
-    . . 5 5 . . 
-    `, SpriteKind.Player)
-let nemeny = sprites.create(img`
-    . 2 2 2 . 
-    2 2 2 2 2 
-    2 2 2 2 2 
-    . 2 2 2 . 
-    `, SpriteKind.Enemy)
-controller.moveSprite(player2, 69, 69)
-nemeny.setPosition(130, 0)
-scene.cameraFollowSprite(player2)
-tiles.setCurrentTilemap(tilemap`5`)
-tiles.setCurrentTilemap(tilemap`3`)
-tiles.setCurrentTilemap(tilemap`2`)
+. . 5 5 . .
+. 5 5 5 5 .
+. 5 5 5 5 . 
+. . 5 5 . . 
+`, SpriteKind.Player)
 
-forever(function () {
-    gazornogaz = randint(1, 69)
+let nemeny = sprites.create(img`
+    . 2 2 2 .
+    2 2 2 2 2
+    2 2 2 2 2
+    . 2 2 2 .
+`, SpriteKind.Enemy)
+
+controller.moveSprite(player2, 69, 69)
+
+forever(function() {
+    let gazornogaz = randint(1, 69)
     nemeny.follow(player2, gazornogaz)
     console.log(gazornogaz)
     pause(1000)
 })
-forever(function(){
-    for (let index = 0; index < 5; index++) {
-        spawnCoin();
-    }
 
-    rotateLevel()
+forever(function() {
+    if (levelCoins >= 5){
+        levelCoins = 0
+        randTilemap()
+        for (let i = 0; i >= 5; i++) {
+            spawnCoin();
+        }
+    }
+})
+
+nemeny.setPosition(130, 0)
+scene.cameraFollowSprite(player2)
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function(player2, nemeny) {
+    //game.gameOver(false)
+})
+
+tiles.setCurrentTilemap(tilemap`1`)
+
+let coins = 0
+let coin: Sprite = null
+let levelCoins = 0
+
+namespace SpriteKind {
+    export const Coin = SpriteKind.create()
+}
+    
+function spawnCoin() {
+    let coin = sprites.create(img`
+            . . 4 4 . .
+            . 4 f f 4 .
+            4 4 f 4 4 4
+            . 4 f f 4 .
+            . . 4 4 . .
+    `, SpriteKind.Coin)
+    tiles.placeOnRandomTile(coin, sprites.castle.tileGrass2)
+}
+
+function randTilemap() {
+    switch (randint(1, 5)) {
+        case 1:
+        tiles.setCurrentTilemap(tilemap`1`)
+
+        case 2:
+        tiles.setCurrentTilemap(tilemap`2`)
+
+        case 3:
+        tiles.setCurrentTilemap(tilemap`3`)
+
+        case 3:
+        tiles.setCurrentTilemap(tilemap`4`)
+
+        case 4:
+        tiles.setCurrentTilemap(tilemap`4`)
+
+        case 5:
+        tiles.setCurrentTilemap(tilemap`5`)
+
+    }
+}
+
+for (let i = 0; i < 5; i++) {
+    spawnCoin()
+}
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Coin, function(player2, coin){
+    coin.destroy()
+    coins++
+    levelCoins++
 })
